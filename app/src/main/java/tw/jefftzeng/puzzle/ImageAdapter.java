81ed36db671d3f979e8bd0c7fbe5549a82ba40be
曾勝億 <jeff810123@gmail.com>
2018-02-07 11:46:21 +0800

    Minimize gap between gridview items

--- app/src/main/java/tw/jefftzeng/puzzle/ImageAdapter.java
+++ app/src/main/java/tw/jefftzeng/puzzle/ImageAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.GridLayout;
 import android.widget.GridView;
@@ -15,6 +18,8 @@ import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
  * Created by JeffTseng on 2018/2/5.
@@ -41,12 +46,20 @@ public class ImageAdapter extends BaseAdapter {
 
 
         Resources res = mContext.getResources();
+        Display display = ((WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
         Bitmap srcBmp = BitmapFactory.decodeResource(res, R.drawable.background);
-        int hScale = (int)(srcBmp.getWidth() / 3);
-        int vScale = (int)(srcBmp.getHeight() / 3);
-        for(int v =0; v <srcBmp.getHeight(); v+=vScale) {
-            for (int h = 0; h < srcBmp.getWidth(); h += hScale) {
-                Bitmap dstBmp = Bitmap.createBitmap(srcBmp, h, v, hScale, vScale);
+        Bitmap resizeBmp = null;
+        double radio = 1.0f;
+        int width = (int)((float)size.x);
+        radio = width/srcBmp.getWidth();
+        resizeBmp = Bitmap.createScaledBitmap(srcBmp, width, (int)(radio * srcBmp.getHeight()), false);
+        int vScale = (int)((double)resizeBmp.getHeight() / 3);
+        int hScale = (int)((double)resizeBmp.getWidth() / 3);
+        for(int v =0; v <resizeBmp.getHeight(); v+=vScale) {
+            for (int h = 0; h < resizeBmp.getWidth(); h += hScale) {
+                Bitmap dstBmp = Bitmap.createBitmap(resizeBmp, h, v, hScale, vScale);
                 splitImages.add(dstBmp);
 
                 ImageView imageView = new ImageView(mContext);
--- app/src/main/java/tw/jefftzeng/puzzle/MainActivity.java
+++ app/src/main/java/tw/jefftzeng/puzzle/MainActivity.java
@@ -2,17 +2,23 @@ package tw.jefftzeng.puzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 public class MainActivity extends AppCompatActivity {
     GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         gridView = (GridView)findViewById(R.id.gridView);
         gridView.setNumColumns(3);
+        //gridView.setVerticalSpacing(16);
+        gridView.setHorizontalSpacing(0);
         gridView.setAdapter(new ImageAdapter(this));
 
     }
